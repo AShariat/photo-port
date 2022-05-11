6d58c3db564c1b21fe0387c2a180c05178bbbcfe
Amir Shariat <amirshariat1987@gmail.com>
2022-05-11 01:06:06 -0600

    PhotoList component created

--- src/App.js
+++ src/App.js
@@ -28,10 +28,8 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
-          <About></About>
-        </div>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
