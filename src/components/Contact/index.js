944673c92b9ace240b1354932a61ce3d4c45a407
Amir Shariat <amirshariat1987@gmail.com>
2022-05-17 20:44:19 -0600

    done with contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
 function ContactForm() {
   const [formState, setFormState] = useState({
@@ -7,10 +8,29 @@ function ContactForm() {
     message: "",
   });
   const { name, email, message } = formState;
+  const [errorMessage, setErrorMessage] = useState("");
 
   // This function will sync the internal state of the component formState with the user input from the DOM. The onChange event listener will ensure that the handleChange function fires whenever a keystroke is typed into the input field for name. In the preceding function, we're using the setFormState function to update the formState value for the name property. We assign the value taken from the input field in the UI with e.target.value and assign this value to the property formState.name. We use the spread operator, ...formState, so we can retain the other key-value pairs in this object. Without the spread operator, the formState object would be overwritten to only contain the name: value key pair.
   function handleChange(e) {
-    setFormState({ ...formState, [e.target.name]: e.target.value });
+    // This conditional statement says if the <input> is email, then validate the value of that input field with the validateEmail function and assign its return to isValid.
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage("Your email is invalid!");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required!`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
   }
 
   function handleSubmit(e) {
@@ -21,36 +41,43 @@ function ContactForm() {
   // Due to keywords reserved in JavaScript, we need to replace the for attribute in the <label> element to htmlFor, just as class had to be changed to className previously.
   return (
     <section>
-      <h1>Contact me</h1>
+      <h1 data-testid="h1tag">Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
           <input
             type="text"
-            defaultValue={name}
-            onChange={handleChange}
             name="name"
+            defaultValue={name}
+            onBlur={handleChange}
           />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
           <input
             type="email"
-            defaultValue={email}
             name="email"
-            onChange={handleChange}
+            defaultValue={email}
+            onBlur={handleChange}
           />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
           <textarea
             name="message"
-            defaultValue={message}
-            onChange={handleChange}
             rows="5"
+            defaultValue={message}
+            onBlur={handleChange}
           />
         </div>
-        <button type="submit">Submit</button>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button data-testid="button" type="submit">
+          Submit
+        </button>
       </form>
     </section>
   );
